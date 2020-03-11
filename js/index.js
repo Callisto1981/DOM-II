2b267a229249bd87bddd6a41ba731be6f88771ef
Wayne <wabbruscatoorr@gmail.com>
2020-03-11 08:47:34 -0400

    updating event listeners. Added mouseiver and dblclick

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+homeLink.addEventListener("dblclick", () => {
+  homeLink.classList.toggle("large");
+});
+
+document.querySelector("nav").addEventListener(
+  "mouseover",
+  () => {
+    event.target.style.color = "red";
+
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  },
+  false
+);
+
+blogLink.addEventListener("mouseover", () => {
+  blogLink.cla;
+});
